6d452c9155876af75b81ee01495981ddd509171a
huanglijun <huanglijun317@gmail.com>
2020-11-02 11:39:17 +0800

    "修改redio的名称"

--- src/biz/cms/pages/form/radio.vue
+++ src/biz/cms/pages/form/radio.vue
@@ -2,9 +2,9 @@
   <div id="page-radio"
        class="page-radio">
     <el-radio v-model="radio"
-              label="1">备选项</el-radio>
+              label="1">备选项1</el-radio>
     <el-radio v-model="radio"
-              label="2">备选项</el-radio>
+              label="2">备选项2</el-radio>
   </div>
 </template>
 
