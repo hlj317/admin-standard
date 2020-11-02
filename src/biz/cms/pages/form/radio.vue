d6f472e1471349279ba4d53f473a66297eeb5253
huanglijun <huanglijun317@gmail.com>
2020-11-02 15:18:14 +0800

    分支冲突测试

--- src/biz/cms/pages/form/radio.vue
+++ src/biz/cms/pages/form/radio.vue
@@ -2,9 +2,9 @@
   <div id="page-radio"
        class="page-radio">
     <el-radio v-model="radio"
-              label="1">备选项13</el-radio>
+              label="1">备选项17</el-radio>
     <el-radio v-model="radio"
-              label="2">备选项14</el-radio>
+              label="2">备选项18</el-radio>
   </div>
 </template>
 
